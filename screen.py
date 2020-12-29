58d2c476ef9260b9e34f81db9574ea5bdcf6b29e
Gods-Virus-666 <62434533+Gods-Virus-666@users.noreply.github.com>
2020-12-29 05:19:52 -0600

    Add files via upload

---
+++ screen.py
@@ -0,0 +1,21 @@
+#You are creating a program that can automatically detect what screen resolution a user is using
+from pynput.mouse import Button, Controller
+mouse = Controller()
+A = (0,999999)
+B = (999999,0)
+mouse.position = A
+C = str(mouse.position)
+mouse.position = B
+D = str(mouse.position)
+E=(C[4:-1])
+F=int(E)
+G=(D[:-4])
+H=(G[1:])
+I=int(H)
+J=str(I+1)
+K=str(F+1)
+print("Screen Resolution is: "+J+"x"+K)
+
+
+
+
